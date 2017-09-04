a763de5c2d9b001b605dff4a2dfacb52e91f8d98
Vitor <vitorvqz@gmail.com>
2017-09-04 09:38:19 -0300

    First

---
+++ banco/useBike_create.sql
@@ -0,0 +1,26 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2017-09-04 12:35:00.826
+
+-- tables
+-- Table: bks_bike
+CREATE TABLE bks_bike (
+    bks_id integer NOT NULL CONSTRAINT bks_bike_pk PRIMARY KEY,
+    bks_identificacao varchar(20) NOT NULL,
+    usr_id integer NOT NULL,
+    bks_latitude varchar(30) NOT NULL,
+    bks_lagitude varchar(30) NOT NULL,
+    CONSTRAINT bks_bike_usr_usuario FOREIGN KEY (usr_id)
+    REFERENCES usr_usuario (usr_id)
+);
+
+-- Table: usr_usuario
+CREATE TABLE usr_usuario (
+    usr_id integer NOT NULL CONSTRAINT usr_usuario_pk PRIMARY KEY,
+    usr_username varchar(50),
+    usr_email varchar(100),
+    usr_password varchar(20),
+    usr_dt_nascimento datetime
+);
+
+-- End of file.
+
