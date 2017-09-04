30d26a53899b55efbf83a7fa684a155256ce4e70
Vitor <vitorvqz@gmail.com>
2017-09-04 09:47:02 -0300

    Script correto Banco

--- banco/useBike_create.sql
+++ banco/useBike_create.sql
@@ -1,14 +1,15 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2017-09-04 12:35:00.826
+-- Last modification date: 2017-09-04 12:46:21.139
 
 -- tables
 -- Table: bks_bike
 CREATE TABLE bks_bike (
     bks_id integer NOT NULL CONSTRAINT bks_bike_pk PRIMARY KEY,
-    bks_identificacao varchar(20) NOT NULL,
+    bks_identificacao varchar(20),
     usr_id integer NOT NULL,
-    bks_latitude varchar(30) NOT NULL,
-    bks_lagitude varchar(30) NOT NULL,
+    bks_latitude varchar(30),
+    bks_lagitude varchar(30),
+    bks_disponivel boolean,
     CONSTRAINT bks_bike_usr_usuario FOREIGN KEY (usr_id)
     REFERENCES usr_usuario (usr_id)
 );
