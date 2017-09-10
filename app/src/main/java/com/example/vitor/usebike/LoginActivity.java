9e7af1f5704038fc6be7aa5dee6b3ed09da3300c
Vitor <vitorvqz@gmail.com>
2017-09-10 13:44:47 -0300

    Consumindo API

--- app/src/main/java/com/example/vitor/usebike/LoginActivity.java
+++ app/src/main/java/com/example/vitor/usebike/LoginActivity.java
@@ -1,7 +1,18 @@
 package com.example.vitor.usebike;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +20,68 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        System.out.println("TESTE API");
+
+        new JSONTask().execute("http://192.168.1.120:3000/usuarios");
+
     }
+
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            System.out.println(result);
+        }
+
+    }
+
 }
+
+
+
